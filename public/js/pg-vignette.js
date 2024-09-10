a9a2cf50558fe3a0486b8546543f69f96f37a15a
Michel Jolivet <michel.jolivet@airbus.com>
2024-09-10 16:27:15 +0200

    Fixed JS error when creating vignette.

--- public/js/pg-vignette.js
+++ public/js/pg-vignette.js
@@ -1,50 +1,5 @@
 (function( $ ) {
 	'use strict';	
-	// $(document).ready(function(){       
-        
-    //     //console.log( "pg-vignettes ready");
-    //     // load all vignettes
-    
-    // }); // end document ready
-
-    // var g_world_data=null;
-
-    // $('#select-country').on('input', function (e){
-    //     //console.log("selection", $('#select-country'));
-    //     // get the selected file name
-    //     let selectedValue = $('#select-country').val();
-    //     // console.log("selection", selectedValue);
-
-    //     if (selectedValue == "None") {
-    //         let mapId = "leaflet-map";
-    //         pg_remove_vignette( mapId);
-    //     }
-    //     else {
-    //         pg_handle_country( selectedValue);
-    //     }
-
-    // });
-
-    // function pg_refresh_marker() {
-    //     //let postId = document.getElementById("post_ID").value;
-    //     let lat = document.getElementById("latitude")?.value;
-    //     let lon = document.getElementById("longitude")?.value;
-
-    //     pg_update_marker_point(lat, lon);
-    // }
-
-    // function pg_delete_markers() {
-    //     //console.log("g_lmap", g_lmap);
-    //     g_lmap?.eachLayer(function (layer) { 
-    //         //console.log("layer", layer);
-    //         // find the layer with latlng
-    //         if (layer._leaflet_id != undefined && layer._latlng != undefined) {
-    //             //console.log("FOUND IT !!!!");
-    //             //layer.setLatLng([newLat,newLon])
-    //             g_lmap.removeLayer(layer);
-    //         } 
-    //     });
-    // }
 
     function pg_add_marker_point( lmap, latitude, longitude) {
         //console.log("pg_add_marker_point country", {lmap, latitude, longitude});
@@ -66,29 +21,6 @@
         }
 
     }
-
-    // function pg_update_marker_point( latitude, longitude) {
-    //     pg_delete_markers();
-    //     if (latitude && longitude) {
-
-    //         pg_add_marker_point(latitude, longitude);
-    //     }
-    // }    
-
-    // function pg_remove_vignette( mapId) {
-    //     // console.log("pg_remove_vignette IN", mapId);
-    //     let previous_map = document.getElementById(mapId);
-    //     if (previous_map) {
-    //         // console.log("pg_remove_vignette", previous_map);
-    //         previous_map.style.display="none";
-    //         //previous_map?.remove();
-    //         if (g_lmap && g_lmap.remove) {
-    //             g_lmap.off();
-    //             g_lmap.remove();
-    //             g_lmap = null;
-    //         }
-    //     }        
-    // }
 
     function pg_add_leaflet_map( elemDiv, mapId, country) {
         // console.log("pg_add_leaflet_map country", {mapId, country});
@@ -171,27 +103,14 @@
             const filename = elem.getAttribute('data-country');
             //const id = elem.id;
             //console.log( "pg_add_vignette_to_slider id", id);
-            const mapId = "vignette-slider-" + elem.id;
+            const mapId = elem.id;
             pg_add_vignette(elem, mapId, filename, lon, lat);
         }
     }
 
-    // window.pg_add_vignette_to_lightbox = function(elem) {
-    //     console.log( "pg_add_vignette_to_lightbox IN", elem);
-
-    //     //console.log( "pg_add_vignette_to_slider IN", elem);
-    //     const lat = elem.getAttribute('data-lat');
-    //     const lon = elem.getAttribute('data-lon');
-    //     const filename = elem.getAttribute('data-country');
-    //     //const id = elem.id;
-    //     const mapId = "vignette-lb-" + elem.id;
-    //     //console.log( "pg_add_vignette_to_lightbox id", id);
-    //     pg_add_vignette(elem, mapId, filename, lon, lat);
-    // }
-
     // handle map vignette    
     function pg_add_vignette (elemDiv, mapId, filename, lon, lat) {
-        console.log("pg_add_vignette IN ", {elemDiv, filename, lon, lat});
+        //console.log("pg_add_vignette IN ", {elemDiv, filename, lon, lat});
         //pg_remove_vignette( mapId)
         if (elemDiv.childElementCount > 0) {
             console.log("pg_add_vignette map already added");
@@ -218,39 +137,6 @@
                     }
                 });
             });
-        // }
-        // else {
-        //     g_world_data.forEach(function (boundary) {
-
-        //         if (boundary.file == filename) {
-        //             let lmap = pg_add_leaflet_map(mapId, boundary);
-        //             // let lat = document.getElementById("latitude")?.value;
-        //             // let lon = document.getElementById("longitude")?.value;
-            
-        //             if (lmap) {
-        //                 pg_add_marker_point(lmap, lat, lon);
-        //             }
-        //         }
-        //     });
-        // }
     }
-
-    // window.pg_load_vignettes = function () {
-    //     console.log( "pg_load_vignettes IN");
-    //     let vignettes = document.getElementsByClassName('pg-descr-vignette');
-    //     for (let i = 0; i < vignettes.length; i++) {
-    //         //console.log( "found vignette", vignettes[i]);
-    //         const lat = vignettes[i].getAttribute('data-lat');
-    //         const lon = vignettes[i].getAttribute('data-lon');
-    //         const filename = vignettes[i].getAttribute('data-country');
-    //         const id = vignettes[i].id;
-    //         //console.log( "pg_load_vignettes id", id);
-    //         pg_add_vignette(id, filename, lon, lat);
-    //     }
-    // }
-
-
-    // let selectedValue = $('#select-country').val();
-    // pg_handle_country(selectedValue);
     
 })( jQuery );
